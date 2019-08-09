e680778376372b0a8cf00ec6c27d66cabec18b53
h-sooah <s2sooas2@ewhain.net>
2019-08-10 04:54:57 +0900

    heroku

--- fourthproject/settings.py
+++ fourthproject/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 CKEDITOR_UPLOAD_PATH = "uploads/"
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -153,3 +154,8 @@ EMAIL_HOST_USER = 'ensk960405@gmail.com'
 EMAIL_HOST_PASSWORD = 'wksmswnd93'
 SERVER_EMAIL = 'ensk960405@gmail.com'
 DEFAULT_FROM_MAIL = 'ensk960405'
+
+# Heroku : Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
